f268aef57c16c7d54a392e9080b7ca2fa0fc748d
Kanchan Sanyal <kanchan_sanyal11@yahoo.com>
2024-03-29 22:59:43 +0530

    Primary header created @ br-1

---
+++ src/components/PrimaryHeader/PrimaryHeader.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import style from './PrimaryHeader.module.css'
+import { Link, NavLink } from 'react-router-dom'
+
+export default function PrimaryHeader() {
+  return (
+    <nav className={style.container}>
+      <div className={`${style['sub-container']} ${style.first}`}>
+        <Link to='/'>Alpha Events</Link>
+      </div>
+      <div className={`${style['sub-container']} ${style.middle}`}>
+        <form className={style['search-form']}>
+          <input type="search" name="search" id="search" />
+          <button>Search</button>
+        </form>
+        <div className={style.menu}>
+          <NavLink to='events' className={({isActive}) => isActive ? style.active : ''}>Events</NavLink>
+          <NavLink to='posts' className={({isActive}) => isActive ? style.active : ''}>Posts</NavLink>
+          <NavLink to='newsletter' className={({isActive}) => isActive ? style.active : ''}>Newsletter</NavLink>
+        </div>
+      </div>
+      <div className={`${style['sub-container']} ${style.last}`}>
+        <NavLink to='login' className={({isActive}) => isActive ? style.active : ''}>Login</NavLink>
+        <form className={style['newsletter-form']}>
+          <input type="email" name="newsletter" id="newsletter" />
+          <button>Register</button>
+        </form>
+      </div>
+    </nav>
+  )
+}
