1e459a730b5c0f6e9f422135f15226ab066ce9da
Kanchan Sanyal <kanchan_sanyal11@yahoo.com>
2024-04-06 02:48:10 +0530

    Reset newsletter input after every form submission

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useFetcher } from 'react-router-dom'
 import style from './RegisterForm.module.css'
 
 export default function RegisterForm() {
 
+  const formRef = useRef()
+
   const fetcher = useFetcher()
 
+
+  useEffect(() => {                     ///////////////// Reseting input data 
+
+    if(fetcher.formData){
+      formRef.current.reset()
+    }
+
+  }, [fetcher.formData])
+
   return (
-    <fetcher.Form method='POST' action='/newsletter' className={style['newsletter-form']}>
+    <fetcher.Form method='POST' action='/newsletter' className={style['newsletter-form']} ref={formRef}>
         <input type="email" name="newsletter"/>
         <button>Register</button>
     </fetcher.Form>
