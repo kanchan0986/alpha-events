399452254227ca422e1fab54dd9b6129bd4fbc3b
Kanchan Sanyal <kanchan_sanyal11@yahoo.com>
2024-04-17 00:59:33 +0530

    Success Modal fixed and re-applied to new form

--- src/components/modals/SuccessModal/SuccessModal.jsx
+++ src/components/modals/SuccessModal/SuccessModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from './SuccessModal.module.css'
+import Portal from '../../Portal/Portal'
 import { useLocation, useSubmit } from 'react-router-dom'
 
 export default function SuccessModal({ message }) {
@@ -8,16 +9,16 @@ export default function SuccessModal({ message }) {
 
   const pathName = useLocation().pathname
 
-  const handleClick = () => { 
+  const handleClose = () => { 
     submit(null, {method: 'POST', action: `${pathName}/success`, replace: true})
    }
 
   return (
-    <div className={style.overlay}>
-        <div className={style.modal}>
+    <Portal handleClose={handleClose}>
+        <div className={style.container}>
             <h2>{message}</h2>
-            <button onClick={handleClick}>Close</button>
+            <button onClick={handleClose}>Close</button>
         </div>
-    </div>
+    </Portal>
   )
 }
